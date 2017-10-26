d1768823c27bf747b13f79bec3596679a486e3a2
Oleksiy Chechel <night-codes@users.noreply.github.com>
2017-10-26 21:10:07 +0300

    Revert 511cc7e03

--- events.go
+++ events.go
@@ -92,7 +92,7 @@ func (e *Event) Emit(args ...interface{}) *Event {
 	e.Lock()
 	defer e.Unlock()
 	for _, v := range e.listeners {
-		go v.fn(args...)
+		v.fn(args...)
 		if !v.once {
 			listeners = append(listeners, v)
 		}
